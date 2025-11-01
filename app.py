18e6afe70f5503bb47b3a75b204e2b5b7c7a05bd
hanis-khairudin <s22a0031@siswa.umk.edu.my>
2025-11-01 15:26:15 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="📊 SciVis Assignment 2025", layout="wide")
+
+st.title("📊 Scientific Visualization Assignment 2025")
+st.write("Welcome! Choose a page from the sidebar to explore each objective 🚀")
+
+st.sidebar.success("⬅️ Select an Objective page")
